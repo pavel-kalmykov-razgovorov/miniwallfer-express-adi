2e403089413b4c9c1efefc5adc28dc97ce27bc92
Pavel Razgovorov <paveltrufi@hotmail.com>
2017-10-26 22:57:14 +0200

    Added some Swagger's Model definitions

--- src/swaggerSpec.ts
+++ src/swaggerSpec.ts
@@ -2,18 +2,57 @@ import SwaggerJSDoc = require("swagger-jsdoc")
 
 const swaggerDefinition = {
     info: {
+        version: "0.1.1",
         title: "Mini Wallfer",
         description: "Minimalistic implementation of the Wallfer platform for colleges",
         contact: {
-            name: "API Support",
+            name: "Pavel Razgovorov",
             email: "pr18@alu.ua.es",
         },
         license: {
-            name: "Apache 2.0",
-            url: "http://www.apache.org/licenses/LICENSE-2.0.html",
+            name: "MIT",
+            url: "http://github.com/gruntjs/grunt/blob/master/LICENSE-MIT",
         },
-        version: "0.1.1",
-    }, securityDefinitions: {
+        schemes: ["http"],
+        consumes: ["application/json"],
+        produces: ["application/hal+json"],
+    },
+    definitions: {
+        User: {
+            allOf: [
+                {
+                    $ref: "#/definitions/NewUser",
+                },
+                {
+                    title: "User",
+                    properties: {
+                        id: { type: "integer" },
+                    },
+                    required: ["id"],
+                },
+            ],
+        },
+        NewUser: {
+            type: "object",
+            description: "A new (not registered) user wich doesn't have posts nor an ID",
+            properties: {
+                username: { type: "string" },
+                password: { type: "string" },
+                firstName: { type: "string" },
+                lastName: { type: "string" },
+                birthdate: { type: "string" },
+            },
+            required: ["username", "password", "firstName", "lastName", "birthdate"],
+        },
+        ApiError: {
+            type: "object",
+            properties: {
+                message: { type: "string" },
+            },
+            required: ["message"],
+        },
+    },
+    securityDefinitions: {
         jwt : {
             type: "apiKey",
             name: "Authorization",
